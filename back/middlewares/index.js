a62036c0bafb1383036175f4f9ec215b64de011e
cfrancoi <cfrancoi@student.42lyon.fr>
2023-09-30 17:37:07 +0200

    fix: mongodb conect
    - add authSource to url
    - mongoos use then/catch instead callback

--- back/middlewares/index.js
+++ back/middlewares/index.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 const authJwt = require("./authJwt");
 const verifySignUp = require("../middlewares/verifySignUp");
-=======
-
-const authJwt = require("./authJwt");
-const verifySignUp = require("./verifySignUp");
->>>>>>> 9779145dd19b8d63994ebf03100c42f8414e48f5
 
 module.exports = {
   authJwt,
