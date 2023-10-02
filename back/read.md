6792d7d66ec7c268c795b58847a421ca138c70c1
wperu <56447404+wperu@users.noreply.github.com>
2023-10-02 15:11:52 +0200

    feat: back init express.js | jwt auth | mongodb
    * add repository back and file package
    
    * feat: init back-end
    
    * chore: add link
    
    * add file for init auth
    
    * add repository back and file package
    
    * feat: init back-end
    
    * chore: add link
    
    * add file for init auth
    
    * add file for connection server and bdd
    
    * update
    
    * fix: mongodb conect
    
    - add authSource to url
    - mongoos use then/catch instead callback
    
    * refactor: del comment
    
    ---------
    
    Co-authored-by: cfrancoi <cfrancoi@student.42lyon.fr>

---
+++ back/read.md
@@ -0,0 +1,14 @@
+autentification jwt (json web token)
+https://www.loginradius.com/blog/engineering/guest-post/nodejs-authentication-guide/
+
+
+<<<<<<< HEAD
+[mongoose express](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose)
+
+=======
+>>>>>>> 9779145dd19b8d63994ebf03100c42f8414e48f5
+lib bdd
+hash et mdp(bcrypte)
+websocket|socket.io
+test
+security/gward/role
---
+++ back/server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require("cors");
+
+const app = express()
+
+var corsOptions ={
+  origin:"http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//simple route
+app.get('/', (req, res) => {
+  res.json({message : "Hello adventurer welcome to project!"})
+})
+
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+
+//set port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+})
+
+const db = require("./models");
+const Role = db.role;
+
+const dbConfig = require("./config/db.config.js");
+
+//TODO FIX passzord
+db.mongoose
+  .connect(`mongodb://root:example@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}?authSource=admin`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  function initial() {
+    Role.estimatedDocumentCount()
+      .then(count => {
+        
+      
+        if (count === 0) {
+          new Role({
+            name: "user"
+          }).save()
+            .then(() => console.log("added 'moderator' to roles collection"))
+            .catch(err => {
+                console.log("error", err);
+            });
+    
+          new Role({
+            name: "moderator"
+          }).save()
+            .then(() => console.log("added 'moderator' to roles collection"))
+            .catch(err => {
+                console.log("error", err);
+            });
+    
+    
+          new Role({
+            name: "admin"
+          }).save()
+            .then(() => console.log("added 'admin' to roles collection"))
+            .catch(err => {
+                console.log("error", err);
+            });
+    
+        }
+      })
+    }
