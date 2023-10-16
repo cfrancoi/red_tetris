df8a5da98e925ade8d1de44fea7e1e2495315d11
wperu <wperu62@gmail.com>
2023-10-16 14:33:52 +0200

    feat: add room with create,join,delete and leave

--- back/socket/socket.js
+++ back/socket/socket.js
@@ -11,11 +11,28 @@ module.exports = function(server) {
 
     io.on('connection', (socket) => {
         console.log('a user connected');
+
+        require('./user.gateway')(socket);
+        require('./room.gateway')(io, socket);
+        socket.on("hello", (arg) => {
+          console.log(arg); // world
+        });
+
+        socket.on("disconnecting", () => {
+
+
+          console.log(socket.rooms); // the Set contains at least the socket ID
+          socket.rooms.forEach(room => {
+            socket.leave(room);
+            console.log('leave room:' + room);
+          })
+        });
+        
         socket.on('disconnect', () => {
           console.log('user disconnected');
         });
       });
 
-  require('./user.gateway')(io);
 }
+
 
