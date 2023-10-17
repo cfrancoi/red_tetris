0a55f972c7d703812a4e157f7e3398442e743a93
wperu <wperu62@gmail.com>
2023-10-17 17:15:29 +0200

    feat: add in room.gateway event (leaveRoom,updateRoom and startRoom)

--- back/socket/room.gateway.js
+++ back/socket/room.gateway.js
@@ -1,13 +1,36 @@
-
 module.exports = function (socket, roomManager) {
-    socket.on('requestRoom', (arg) => {
-        const room = roomManager.addPlayerToRoom(arg, socket);
+  socket.on('requestRoom', (arg) => {
+    const room = roomManager.addPlayerToRoom(arg, socket);
 
-        if (room) {
-            socket.join(room.id);
-            socket.to(room.id).emit('roomJoined', room.toJSON())
-            socket.emit('roomJoined', room.toJSON())
-            console.log(room);
-        }
-    });
+    if (room) {
+      socket.join(room.id);
+      socket.to(room.id).emit('roomJoined', room.toJSON())
+      socket.emit('roomJoined', room.toJSON())
+      console.log(room);
+
+    }
+  });
+
+  socket.on('startRoom', (arg) => {
+    const room = roomManager.
+      if(room){
+
+      }
+
+  });
+
+  socket.on('leaveRoom', (arg) => {
+    const room = roomManager.removePlayerFromRoom(arg, socket);
+
+    if (room) {
+      socket.leave(room.id)
+      socket.to(room.id).emit('roomLeft', { roomId: room.id, playerId: socket.id });
+      socket.emit('roomLeft', { roomId: room.id, playerId: socket.id });
+      console.log(room);
+    }
+  });
+
+  socket.on('updateRoom', (arg) => {
+
+  });
 }
