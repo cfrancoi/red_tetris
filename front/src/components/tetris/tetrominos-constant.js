58b62c5841fb896a75896a28e6689fa44afb565f
wperu <wperu62@gmail.com>
2023-10-31 15:17:36 +0100

    add tetrominos J,S,Z,T

--- front/src/components/tetris/tetrominos-constant.js
+++ front/src/components/tetris/tetrominos-constant.js
@@ -11,7 +11,28 @@ export const L_TETROMINO = [
     [0, 'L', 'L'],
 ]
 
+export const J_TETROMINO = [
+    [0, 'J', 0],
+    [0, 'J', 0],
+    ['J', 'J', 0],
+]
+
 export const O_TETROMINO = [
     ['O', 'O'],
     ['O', 'O']
+]
+
+export const T_TETROMINO = [
+    ['T', 'T', 'T'],
+    [0, 'T', 0],
+]
+
+export const S_TETROMINO = [
+    [0, 'S', 'S'],
+    ['S', 'S', 0],
+]
+
+export const Z_TETROMINO = [
+    ['Z', 'Z', 0],
+    [0 , 'Z', 'Z'],
 ]
