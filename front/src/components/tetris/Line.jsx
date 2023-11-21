4dcdec41574bd6dc489af307aaafe45664795b82
cfrancoi <corentinfrancois31@gmail.com>
2023-11-21 17:43:29 +0100

    fix: add preview css style

--- front/src/components/tetris/Line.jsx
+++ front/src/components/tetris/Line.jsx
@@ -5,8 +5,9 @@ export default function Line({ line }) {
   return (
     <div className='line'>
       {line.map((cell, index) => {
+        const preview = (cell.preview) ? 'preview' : '';
         return (
-          <div className={`defaultCell ${(cell.type != 'default') ? cell.type + 'Cell' : ''}`} key={index}>
+          <div className={`defaultCell ${(cell.type != 'default') ? cell.type + 'Cell' : ''} ${preview}`} key={index}>
             {/* {cell.color} */}
           </div>
         )
