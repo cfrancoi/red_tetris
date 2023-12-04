3d570f0cd506794f824a6a90c2944500297ba67c
cfrancoi <corentinfrancois31@gmail.com>
2023-12-04 16:56:30 +0100

    chore: tertis change action key

--- front/src/components/tetris/TetrisBoard.jsx
+++ front/src/components/tetris/TetrisBoard.jsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useSocket } from '../../context/SocketContext'
 import Line from './Line';
+import { EKEYCODE } from '../../utils/EKEYCODE';
 
 const EMOVE = {
   DOWN: 'moveDown',
@@ -14,8 +15,7 @@ const EMOVE = {
 }
 
 export default function TetrisBoard({ playerId, isControlled }) {
-  const [show, setShow] = useState(false);
-  const tetris = useSelector(state => (state.tetris.players.find(p => p.id === playerId)));
+  const player = useSelector(state => (state.tetris.players.find(p => p.id === playerId)));
   const roomId = useSelector(state => (state.tetris.roomId));
   const { socket } = useSocket();
 
@@ -23,43 +23,43 @@ export default function TetrisBoard({ playerId, isControlled }) {
     socket.emit(moveDirection, roomId);
   }, [socket, roomId]);
 
-  const onKeyPressEvent = useCallback((e) => {
-    if (e.key === 's')
-      move(EMOVE.DOWN);
-    if (e.key === 'd')
-      move(EMOVE.RIGHT);
-    if (e.key === 'a')
-      move(EMOVE.LEFT);
-    if (e.key === ' ')
+  const onKeyDownEvent = useCallback((e) => {
+    if (e.keyCode === EKEYCODE.UP_ARROW) {
       move(EMOVE.ROTATE);
+    }
+    if (e.keyCode === EKEYCODE.DOWN_ARROW) {
+      move(EMOVE.DOWN);
+    }
+    if (e.keyCode === EKEYCODE.RIGHT_ARROW) {
+      move(EMOVE.RIGHT);
+    }
+    if (e.keyCode === EKEYCODE.LEFT_ARROW) {
+      move(EMOVE.LEFT);
+    }
+    if (e.keyCode === EKEYCODE.SPACE) {
+      move(EMOVE.DROP)
+    }
+    
   }, [move]);
 
   useEffect(() => {
     if (isControlled) {
       console.log(' add event')
 
-      window.addEventListener('keypress', onKeyPressEvent);
+      window.addEventListener('keydown', onKeyDownEvent);
     }
     return () => {
       if (isControlled) {
         console.log('remove event');
-        window.removeEventListener('keypress', onKeyPressEvent);
+        window.addEventListener('keydown', onKeyDownEvent);
+
       }
     }
-  }, [isControlled, onKeyPressEvent]);
+  }, [isControlled, onKeyDownEvent]);
 
   return (
-    <div onMouseOver={() => { setShow(true) }}
-      onMouseLeave={() => { setShow(false) }}
-
-      className={show ? 'show' : ''}>
-      {show ? "true" : "flase"}
-      <button onClick={() => { move(EMOVE.DOWN) }}>down</button>
-      <button onClick={() => { move(EMOVE.LEFT) }}>left</button>
-      <button onClick={() => { move(EMOVE.RIGHT) }}>right</button>
-      <button onClick={() => { move(EMOVE.ROTATE) }}>rotate</button>
-      <button onClick={() => { move(EMOVE.DROP) }}>drop</button>
-      {tetris?.grid.map((line, index) => {
+    <div>
+      {player?.grid.map((line, index) => {
         return (
           <div key={index}>
             <Line line={line} isOpponent={!isControlled}></Line>
@@ -67,7 +67,7 @@ export default function TetrisBoard({ playerId, isControlled }) {
         );
       })}
 
-      {`score: ${tetris?.score} playerId: ${playerId} isOpponent:${!isControlled}`}
+      {`score: ${player?.score} playerId: ${playerId} isOpponent:${!isControlled}`}
     </div>
   );
 }
